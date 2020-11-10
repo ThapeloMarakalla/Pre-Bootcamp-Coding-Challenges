c3ebbd7104c498cb7557cbd45352aa0866b01b8d
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-11-10 15:44:27 +0200

    add task3.py

---
+++ task3.py
@@ -0,0 +1,18 @@
+def task3(num1, num2):
+    """Returns True if either num1 or num2 is 65 OR the sum of num1 and num2 
+    is 65, False otherwise.
+    """
+    if (65 in [num1, num2]) or ((num1+num2) == 65):
+        return True
+    return False
+
+if __name__ == '__main__':
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    if task3(num1, num2):
+        print('Either one of the numbers(or both) is 65 or the sum of the', end=' ')
+        print('numbers is 65')
+    else:
+        print('None of the number is 65 and the sum of the', end=' ')
+        print('numbers is not 65')
