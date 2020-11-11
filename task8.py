777603f8ff18a20d702dba7e38a5a4228f91ab15
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-11-11 04:41:05 +0200

    update task8.py

--- task8.py
+++ task8.py
@@ -1,11 +1,14 @@
 def to_standard_time(num):
     """Print num as hours and minutes
     """
-    hr = num // 60
+    hours = num // 60
     num %= 60
-    min = num % 60
+    minutes = num % 60
 
-    print(f'{hr} hours, {min} minutes')
+    if hours == 0:
+        print(f'{minutes} minutes')
+    else:
+        print(f'{hours} hour(s), {minutes} minutes')
 
 if __name__ == '__main__':
     to_standard_time(71)
