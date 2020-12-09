677ed738bca11276d3d040060ce5eb34b3a41086
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-12-09 14:40:38 +0200

    add task5.py

--- task5.py
+++ task5.py
@@ -2,8 +2,11 @@ def calclate_area(side1, side2, side3):
     """Calulate the area of a triangle with side1, side2 and side3
     as the sides of the triangle
     """
-    s = (side1+side2+side3) / 2
-    return (s*(s-side1)*(s-side2)*(s-side3))**.5
+    semi_perimeter = (side1 + side2 + side3) / 2
+    area = (semi_perimeter * (semi_perimeter - side1) *
+            (semi_perimeter - side2) * (semi_perimeter - side3))**.5
+    return area
+
 
 if __name__ == '__main__':
     side1 = 3
