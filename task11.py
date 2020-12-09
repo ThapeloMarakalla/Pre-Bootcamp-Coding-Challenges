517f46fe7caa86fbbc29bebfcbe3f1ee1c1ab541
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-12-09 14:39:53 +0200

    update task11.py

--- task11.py
+++ task11.py
@@ -1,10 +1,6 @@
 def commom_characters(string1, string2):
     """Prints commint characters that common in string1 and string2
     """
-
-    # 1. coverts string1 and string2 to two sets of characters,
-    #  to remove duplicates.
-    # 2. use set operator & to find common elements in both sets.
     print('Common letters:', *(set(string1) & set(string2)))
 
 if __name__ == '__main__':
