2b5e780fc7aafc9c3f9284be8c861fe55bf4933b
Thapelo Marakalla <thapelomarakalla@gmail.com>
2020-12-09 14:40:12 +0200

    task4.py

--- task4.py
+++ task4.py
@@ -1,15 +1,16 @@
 def task4(num1, num2):
-    """Returns True if either num1 or num2 is 3, AND the sum of num1 and num2 contains 
+    """Returns True if either num1 or num2 is 3, AND the sum of num1 and num2 contains
     a 3, False otherwise.
     """
     if (3 in [num1, num2]) and ('3' in str(num1 + num2)):
         return True
     return False
 
+
 if __name__ == '__main__':
     num1 = 3
     num2 = 13
-    
+
     if task4(num1, num2):
         print('Either one of the numbers(or both) is 3 and the sum of the', end=' ')
         print('numbers contains a 3')
